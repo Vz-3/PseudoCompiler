2f68e955568f197cb2eb769405630f86575e17fc
Vz-3 <vondc6201@gmail.com>
2023-10-23 13:46:06 +0800

    Refactored enums and tokenizer

--- lexicalAnalyzer.py
+++ lexicalAnalyzer.py
@@ -18,29 +18,20 @@ class atomType(Enum):
     arithemetic = 8
     end = 9
 
-class tokenType(Enum):
-    # Order Matters. Case sensitive for keywords. Arranged in likelihood.
-    KEYWORD_IF = r"if"
-    KEYWORD_OUTPUT = r"output"
-    KEYWORD_INT = r"integer"
-    KEYWORD_DOUBLE = r"double"
 
-    IDENTIFIER = r"[a-zA-Z_][a-zA-Z0-9_]*"
+# Basically these enums are like CFG rules.
+# Base if doesn't contain enums.
+# Extended if part of general.
+# General if part of umbrella and contains other enums.
+# Need the enum names to match the token type names in higher levels otherwise it wont work.
 
-    LITERAL_DOUBLE = r"[0-9]*\.[0-9]*" # Literals are same as constants.
-    LITERAL_INTEGER = r"[0-9]*"
-    LITERAL_STRING = r'''(['"])(.*?)\1''' # This could also be the alternative source of the error. 
+# BASE
+class DELIMETER(Enum):
+    DELIMITER_LEFT_P = r"\("
+    DELIMITER_RIGHT_P = r"\)"
 
-    ENDLINE = r";"
-
-    OP_ASSIGNMENT = r":="
-    OP_COLON = r":" # For the type declaration
-    OP_EQUAL = r"=" # For the start of an operation.
-    OP_LEFTSHIFT = r"<<"  
-    OP_ARITHMETIC_PLUS = r"\+"
-    OP_ARITHMETIC_MINUS = r"-"
-    OP_ARITHMETIC_MULTIPLY = r"\*"
-    OP_ARITHMETIC_DIVIDE = r"/"
+# EXTENDED
+class RELATIONAL(Enum):
     OP_RELATIONAL_ISEQUAL = r"=="
     OP_RELATIONAL_NOTEQUAL = r"!="
     OP_RELATIONAL_LESSTHAN = r"<"
@@ -48,8 +39,64 @@ class tokenType(Enum):
     OP_RELATIONAL_GREATERTHAN = r">"
     OP_RELATIONAL_GREATERTHANOREQUAL = r">="
 
-    DELIMITER_LEFT_P = r"\("
-    DELIMITER_RIGHT_P = r"\)"
+# EXTENDED 
+class ARITHMETIC(Enum):
+    OP_ARITHMETIC_PLUS = r"\+"
+    OP_ARITHMETIC_MINUS = r"-"
+    OP_ARITHMETIC_MULTIPLY = r"\*"
+    OP_ARITHMETIC_DIVIDE = r"/"
+
+# GENERAL
+class OPERATOR(Enum):
+    OP_ASSIGNMENT = r":="
+    OP_COLON = r":" # For the type declaration
+    OP_EQUAL = r"=" # For the start of an operation.
+    OP_LEFTSHIFT = r"<<" 
+    ARITHMETIC = '|'.join([arithmeticOperator.value for arithmeticOperator in ARITHMETIC])
+    RELATIONAL = '|'.join([relationalOperator.value for relationalOperator in RELATIONAL])
+
+# BASE
+class LITERAL(Enum):
+    LITERAL_DOUBLE = r"[0-9]*\.[0-9]*" # Literals are same as constants.
+    LITERAL_INTEGER = r"[0-9]*"
+    LITERAL_STRING = r'''(['"])(.*?)\1''' # This could also be the alternative source of the error. 
+
+# EXTENDED 
+class DATA_TYPE(Enum):
+    KEYWORD_INT = r"integer"
+    KEYWORD_DOUBLE = r"double"
+
+# GENERAL
+class KEYWORD(Enum):
+    DATA_TYPE = '|'.join([dt.value for dt in DATA_TYPE])
+    KEYWORD_IF = r"if"
+    KEYWORD_OUTPUT = r"output"
+
+# UMBRELLA
+class tokenType(Enum):
+    # Order Matters. Case sensitive for keywords. Arranged in likelihood.
+    KEYWORD = '|'.join([keyword.value for keyword in KEYWORD])
+
+    IDENTIFIER = r"[a-zA-Z_][a-zA-Z0-9_]*"
+
+    LITERAL = '|'.join([literal.value for literal in LITERAL])
+
+    ENDLINE = r";"
+
+    OPERATOR = '|'.join([operator.value for operator in OPERATOR])
+
+    DELIMITER = '|'.join([delimiter.value for delimiter in DELIMETER])
+
+# Storing all higher levels. 
+enumDictionary = {
+    "KEYWORD": KEYWORD,
+    "OPERATOR": OPERATOR,
+    "DELIMITER": DELIMETER,
+    "LITERAL": LITERAL,
+    "DATA_TYPE": DATA_TYPE,
+    "RELATIONAL": RELATIONAL,
+    "ARITHMETIC": ARITHMETIC
+}
 
 class LexicalAnalyzer:
     def __init__(self, fileToTokenize) -> None:
@@ -58,16 +105,21 @@ class LexicalAnalyzer:
         with open(fileToTokenize, "r") as file:
             self.file = file.read()
 
-        self.atoms = [] # A nested list containing the atoms of each line.
-        self.tokens = [] # A list containing the tokens of the file in the form of a tuple (token, type).
-        self.symbol_table = {} # Considered as environment
+        self.atoms = [] # A nested list containing the atoms of each line. Should not be modified after atomizer() (R)
+        self.tokens = [] # A list containing the tokens of the file in the form of a tuple (token, type). Can only be changed after parsing, to sort out negative values. (R)
+        self.symbol_table = {} # Considered as environment (RW)
         self.isFirstError = True # Indicates if it is the first error, useful for the error log.
 
+        self.tokensCopy = [] # used for symbol table initialization and perhaps other uses. (RW)
         # The following functions will be called in the constructor.
         self.initDir()
         self.atomizer()
         self.tokenizer()
         self.analyzeTokens()
+
+    def updateTokenCopy(self) -> None:
+        """ This function will update the token copy. Useful for symbol table initialization and perhaps other uses."""
+        self.tokensCopy = self.tokens.copy()
 
     def initDir(self) -> None:
         """ This function will create a directory for the logs."""
@@ -177,6 +229,53 @@ class LexicalAnalyzer:
                     for atom in line:
                         file.write(atom)
                     file.write("\n")
+    def tokenize(self, typeScope, token, lineCount) -> tokenType:
+        """ Smaller version of tokenizer, used for individual token matching!"""
+        print("GOT CALLED HOW BOUT THAT")
+        isToken = False
+        for ttype in typeScope:
+            print("Typescope:", ttype.value, "Token:", token)
+            if re.fullmatch(ttype.value, token):
+                isToken = True
+                print("Found match:", ttype.name, "Token:", token)
+                break
+        print()
+        if not isToken:
+                print("Error on line", lineCount+1, "Token:", token)
+                self.reportError(token, lineCount, "Invalid Data Type.", "Lexical Error")
+
+    # For tokenizer only. 
+    def checkTokenType(self, token, typeScope: Enum) -> (bool, str): 
+        """ This function will check the type of the token, returns the specified type if it matches, else returns typeScope"""
+        isToken = False
+        print("Typescope:", typeScope, "Token:", token, end = "\t\t")
+        
+        nextScope = None
+
+        for ttype in typeScope:
+            if re.fullmatch(ttype.value, token):
+                isToken = True
+                nextScope = ttype.name
+                break
+        if not isToken:
+            # If it reaches this point, this means that there's a regex in the higher scope that matches the token but not the lower scope.
+            return (False, None)
+        print("Found match:", nextScope, "with type ", type(nextScope), end = "\t\t")
+        
+        # Recurse if next scope is in the enum dictionary.
+        for name, enum in enumDictionary.items():
+            # If the next scope: e.g. OPERATOR matches the name of the enum, then we recurse.
+            if nextScope == name:
+                nextScope = enum
+                # the nextScope would properly point to an enum now. 
+                result = self.checkTokenType(token, nextScope)
+                return result
+
+        # Should always be a str data type at this point.
+        print("Returns with type:", nextScope, " of type ", type(nextScope), end="\n\n")
+        return (True, nextScope)
+
+        
 
     def tokenizer(self) -> None:
         """ This function will analyze the tokens and determine the type of each token."""
@@ -192,15 +291,11 @@ class LexicalAnalyzer:
         lineCount = 0
         for line in self.atoms:
             for token in line:
-                isToken = False
-                for ttype in tokenType:
-                    if re.fullmatch(ttype.value, token):
-                        self.tokens.append((token, ttype.name))
-                        isToken = True
-                        break
+                isToken, typeToken = self.checkTokenType(token, tokenType)
                 if not isToken:
                     self.reportError(token, lineCount, "Invalid Token Type.", "Lexical Error")
                     self.tokens.append((token, 'err'))
+                self.tokens.append((token, typeToken))
             lineCount += 1
         
         # Debugging
