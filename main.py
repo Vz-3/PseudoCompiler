3eb16cd6fce825383013453be174731b2d60d1e0
dreyoji <Ryojibernardo@gmail.com>
2023-10-23 23:35:20 +0800

    Created Syntax Analyzer and Grammar Rules
    - AST is not working yet

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import lexicalAnalyzer as lex
+from syntaxAnalyzer import SyntaxAnalyzer
 
 if __name__ == "__main__":
     lex = lex.LexicalAnalyzer("src.txt")
+    #syntax_analyzer = SyntaxAnalyzer(lex)
+    #syntax_analyzer.analyze()
