cfafe67e691de916300b0c1713e9c11943e4d8fb
Paul Reonal <paulmiguel.reonal@gmail.com>
2023-10-24 23:01:35 +0800

    Fixed comments in main

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
 import lexicalAnalyzer as lex
-<<<<<<< Updated upstream
-from syntaxAnalyzer import SyntaxAnalyzer
-=======
 from Parser import Parser
->>>>>>> Stashed changes
 
 if __name__ == "__main__":
     lex = lex.LexicalAnalyzer("src.txt")
-    #parser = Parser.parse(lex.getTokens())
     #print(lex.getTokens())
     tokens = [t[::-1] for t in lex.getTokens()]
     parser = Parser(tokens)
     parser.parse()
-    #print(reversed_tuples)
     #print(parser)
